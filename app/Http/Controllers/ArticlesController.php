f6b23f2d08b28eda25a0a8a84fa7127bd3f8fbdd
sharmilatimsinasapkota <sharmila_timsap@hotmail.com>
2020-09-20 19:39:12 +0100

    form validation

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -21,6 +21,11 @@ class ArticlesController extends Controller
     }
 
     public function store(){
+        request()->validate([
+            'title'=>'required',
+            'excerpt'=>'required',
+            'body'=>'required'
+            ]);
         $article=new Article();
         $article->title=request('title');
         $article->excerpt=request('excerpt');
--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,37 +1,51 @@
 @extends('layout')
 @section('head')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
-@endsection 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+@endsection
 @section('content')
 
-<div id="wrapper">
-    <div id="page" class="container">
-        <h1 class="heading has-text-weight-bold">New Article</h1>
-        <form method="POST" action="/articles">
-            @csrf
-            <div class="field">
-                <label class="label" for="title">Title</label>
-                <div class="control">
-                    <input class="input" type="text" name="title" id="title">
-                </div>
-            </div>
-            <div class="field">
-                <label class="label" for="excerpt">Excerpt</label>
-                <div class="control">
-                    <textarea class="textarea" name="excerpt" id="excerpt"></textarea>
+    <div id="wrapper">
+        <div id="page" class="container">
+            <h1 class="heading has-text-weight-bold">New Article</h1>
+            <form method="POST" action="/articles">
+                @csrf
+                <div class="field">
+                    <label class="label" for="title">Title</label>
+                    <div class="control">
+                        <input class="input  @error('title') is-danger @enderror" type="text" name="title" id="title"
+                            value="{{ old('title') }}">
+                        @error('title')
+                        <p class="help is-danger">{{ $errors->first('title') }}</p>
+                        @enderror
+                    </div>
                 </div>
                 <div class="field">
-                    <label class="label" for="body">Body</label>
+                    <label class="label" for="excerpt">Excerpt</label>
                     <div class="control">
-                        <textarea class="textarea" name="body" id="body"></textarea>
+                        <textarea class="textarea @error('excerpt') is-danger @enderror" name="excerpt" id="excerpt">
+                        {{ old('excerpt') }}
+                        </textarea>
+                        @error('excerpt')
+                        <p class="help is-danger">{{ $errors->first('excerpt') }}</p>
+                        @enderror
                     </div>
-                    <div class="field is-grouped">
+                    <div class="field">
+                        <label class="label" for="body">Body</label>
                         <div class="control">
-                            <button class="button is-link" type="submit">Submit</button>
+                            <textarea class="textarea @error('body') is-danger @enderror" name="body" id="body">
+                            {{ old('body') }}
+                            </textarea>
+                            @error('body')
+                            <p class="help is-danger">{{ $errors->first('body') }}</p>
+                            @enderror
                         </div>
+                        <div class="field is-grouped">
+                            <div class="control">
+                                <button class="button is-link" type="submit">Submit</button>
+                            </div>
+                        </div>
+            </form>
+        </div>
     </div>
-        </form>
-    </div>
-</div>
-    
+
 @endsection
